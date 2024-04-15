ba027bb9d50eb771836e87dd700924cb07bcb1a5
minpeter <minpeter@friendli.ai>
2024-04-15 16:15:27 +0900

    Add new dependencies and update package.json, layout.tsx, globals.css, and tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,128 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { ReloadIcon } from "@radix-ui/react-icons";
+
+import { Button, buttonVariants } from "@/components/ui/button";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+type LinkState = "Checking" | "Valid" | "Invalid";
+
+let storeData: any[] = [];
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [randomData, setRandomData] = useState<any>(null);
+  const [urlState, setUrlState] = useState<LinkState>("Checking");
+
+  useEffect(() => {
+    fetch("/api/resources.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.entries);
+        storeData = data.entries;
+        NewRandom();
+      });
+  }, []);
+
+  function NewRandom() {
+    setLoading(true);
+    setUrlState("Checking");
+
+    const rd = storeData.sort(() => Math.random() - 0.5)[0];
+
+    setRandomData(rd);
+
+    fetch(rd?.Link)
+      .then((res) => {
+        if (res.ok) {
+          setUrlState("Valid");
+        }
+      })
+      .catch(() => {
+        setUrlState("Invalid");
+      });
+
+    setLoading(false);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Card>
+      {loading ? (
+        <CardHeader>
+          <div className="flex flex-col space-y-3">
+            <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        </CardHeader>
+      ) : (
+        <>
+          <CardHeader>
+            <CardTitle>{randomData?.API}</CardTitle>
+            <CardDescription>{randomData?.Description}</CardDescription>
+          </CardHeader>
+          <CardContent className="flex gap-2">
+            {urlState === "Checking" && (
+              <Button disabled>
+                <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            )}
+            {urlState === "Valid" && (
+              <Link
+                href={randomData.Link}
+                target="_blank"
+                className={buttonVariants()}
+              >
+                Check it out
+              </Link>
+            )}
+            {urlState === "Invalid" &&
+              (randomData?.Cors ? (
+                <Link
+                  href={randomData.Link}
+                  target="_blank"
+                  className={buttonVariants({ variant: "destructive" })}
+                >
+                  Plz Manual Check
+                </Link>
+              ) : (
+                <>
+                  <Button disabled>URL is invalid</Button>
+                </>
+              ))}
+            <Button variant="outline" size="icon" onClick={NewRandom}>
+              <ReloadIcon className="h-4 w-4" />
+            </Button>
+          </CardContent>
+          <CardFooter className="flex gap-2">
+            <Badge variant="outline">{randomData?.Category}</Badge>
+            <Badge variant="outline">{randomData?.Auth || "No Auth"}</Badge>
+            <Badge variant="outline">
+              {randomData?.Cors ? "CORS" : "No CORS"}
+            </Badge>
+            <Badge variant="outline">
+              {randomData?.HTTPS ? "HTTPS" : "HTTP"}
+            </Badge>
+          </CardFooter>
+        </>
+      )}
+    </Card>
   );
 }
